d5c62d878d2ff95422746163a9550127a2011cd0
Hoko Le <hokole527@gmail.com>
2019-10-31 17:58:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  array = [one, two, three, four]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << five
 end
 
 def add_element_to_start_of_array(array, element)
